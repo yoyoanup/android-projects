792ecbef32352dcfc06d3ca0a874d94f81206f31
yoyoanup <nanki.anup@gmail.com>
2017-10-31 02:55:46 +0530

    yo oy

--- intent/app/src/main/java/com/example/yoyoanup/intent/act2.java
+++ intent/app/src/main/java/com/example/yoyoanup/intent/act2.java
@@ -4,7 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
-
+//skadwdk
+/dshjk,
 public class act2 extends AppCompatActivity  {
 
     @Override
