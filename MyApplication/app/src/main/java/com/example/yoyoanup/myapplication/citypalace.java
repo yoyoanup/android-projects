762fcf782593f5e8c43f56e8005e7ed4e14284ae
yoyoanup <nanki.anup@gmail.com>
2017-12-03 04:59:49 +0530

    final

--- MyApplication/app/src/main/java/com/example/yoyoanup/myapplication/citypalace.java
+++ MyApplication/app/src/main/java/com/example/yoyoanup/myapplication/citypalace.java
@@ -1,7 +1,11 @@
 package com.example.yoyoanup.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class citypalace extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class citypalace extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citypalace);
 
+
+    }
+
+    public void rest(View view) {
+        String st3="near by atm";
+            Uri location = Uri.parse("geo:0,0?q=" +st3);
+            Intent i2=new Intent(Intent.ACTION_VIEW,location);
+            if (i2.resolveActivity(getPackageManager()) != null) {
+                startActivity(i2);
+            } else {
+                Log.d("ImplicitIntents", "cann not resolve");
+            }
+        }
+
+
+    public void fuel(View view) {
+
+        String st3="Petrol Pump";
+        Uri location = Uri.parse("geo:0,0?q=" +st3);
+        Intent i2=new Intent(Intent.ACTION_VIEW,location);
+        if (i2.resolveActivity(getPackageManager()) != null) {
+            startActivity(i2);
+        } else {
+            Log.d("ImplicitIntents", "cann not resolve");
+        }
     }
 }
+
